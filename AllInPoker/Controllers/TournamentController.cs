53ea63e01c77b9c506cc1740e2f0cf1ef65002c4
ChrisRu <christian@ruigrok.info>
2017-06-09 19:59:15 +0200

    Added insert tournament method

--- AllInPoker/Controllers/TournamentController.cs
+++ AllInPoker/Controllers/TournamentController.cs
@@ -64,12 +64,110 @@
             return tournaments;
         }
 
+        public void CreateTournament(
+            DateTime date,
+            decimal cost,
+            int minPlayers,
+            int minAge,
+            int maxAge,
+            int locationId,
+            int winnerId)
+        {
+            MySqlTransaction trans = null;
+
+            try
+            {
+                this.Connection.Open();
+                trans = this.Connection.BeginTransaction();
+
+                const string InsertString = @"INSERT INTO tournament (date, time, cost, min_players, min_age, max_age, location_id, winner_id) VALUES (@date, @time, @cost, @min_players, @min_age, @location_id, @winner_id)";
+
+                MySqlCommand command =
+                    new MySqlCommand(InsertString, this.Connection)
+                        {
+                            Parameters =
+                                {
+                                    new MySqlParameter(
+                                        "@date",
+                                        MySqlDbType.DateTime)
+                                        {
+                                            Value
+                                                = date
+                                        },
+                                    new MySqlParameter(
+                                        "@time",
+                                        MySqlDbType.Time)
+                                        {
+                                            Value
+                                                = date
+                                                    .ToLocalTime()
+                                        },
+                                    new MySqlParameter(
+                                        "@cost",
+                                        MySqlDbType.Decimal)
+                                        {
+                                            Value
+                                                = cost
+                                        },
+                                    new MySqlParameter(
+                                        "@min_players",
+                                        MySqlDbType.Int64)
+                                        {
+                                            Value
+                                                = minPlayers
+                                        },
+                                    new MySqlParameter(
+                                        "@min_age",
+                                        MySqlDbType.Int64)
+                                        {
+                                            Value
+                                                = minAge
+                                        },
+                                    new MySqlParameter(
+                                        "@location_id",
+                                        MySqlDbType.Int64)
+                                        {
+                                            Value
+                                                = maxAge
+                                        },
+                                    new MySqlParameter(
+                                        "@location_id",
+                                        MySqlDbType.Int64)
+                                        {
+                                            Value
+                                                = locationId
+                                        },
+                                    new MySqlParameter(
+                                        "@winner_id",
+                                        MySqlDbType.Int64)
+                                        {
+                                            Value
+                                                = winnerId
+                                        }
+                                }
+                        };
+
+                command.Prepare();
+                command.ExecuteNonQuery();
+                trans.Commit();
+            }
+            catch (Exception e)
+            {
+                trans?.Rollback();
+                MessageBox.Show("Toernooi toevoegen mislukt. ", e.Message);
+            }
+            finally
+            {
+                this.Connection.Close();
+            }
+        }
+
         /// <summary>
         /// Fetch City Name based on location id
         /// </summary>
         /// <param name="locationId">ID in event_location table</param>
         /// <returns>City Name</returns>
-        private string GetCityName(int locationId)
+        public string GetCityName(int locationId)
         {
             try
             {
